205fb2c83ef47220a9a56014115111f435765ed9
laurenfazah <laurenfazah@gmail.com>
2016-07-25 12:28:05 -0400

    Updated index.js from js-template

--- index.js
+++ index.js
@@ -8,9 +8,12 @@
 require('./assets/scripts/index.js');
 
 // styles
-require('normalize.css');
 require('./assets/styles/index.scss');
 
 // attach jQuery globally
 require('expose?$!jquery');
 require('expose?jQuery!jquery');
+
+// attach getFormFields globally
+
+require('expose?getFormFields!./lib/get-form-fields.js');
